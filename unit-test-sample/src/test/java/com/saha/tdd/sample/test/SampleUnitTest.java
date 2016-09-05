8cb4dd95dc95719d31f014fe877a09aab02e55cc
melih <melih.sakarya@gmail.com>
2016-09-05 19:39:06 +0300

    first unit test

---
+++ unit-test-sample/src/test/java/com/saha/tdd/sample/test/SampleUnitTest.java
@@ -0,0 +1,14 @@
+package com.saha.tdd.sample.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class SampleUnitTest {
+
+	
+	@Test
+	public void sampleTest(){
+		assertEquals(2, 2);
+	}
+}
